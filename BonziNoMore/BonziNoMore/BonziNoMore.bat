6470cf484f2ed988ca40319f090793c9f06815d4
NorkenNort <56931477+NorkenNort@users.noreply.github.com>
2019-10-30 20:21:39 +0000

    V.0.0.1 RELEASE
    The first release of bonzi no more

---
+++ BonziNoMore/BonziNoMore/BonziNoMore.bat
@@ -0,0 +1,9 @@
+cd C:\Program Files (x86)\BonziBuddy432
+echo off
+cls
+echo Press any key to delete bonzi without him knowing!
+pause
+del *
+cd C:\Program Files (x86)\BonziBuddy432/bonziBDY_4.exe
+del *
+echo Bonzi has been deleted!
